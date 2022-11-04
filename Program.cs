2a9900bb2c383fb5e2d9b4560d8c9dd44e01fed7
Diane Morrison <dianemorrison@dianes-mbp.home>
2022-11-04 11:22:42 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;                  // provides Linq Collection extension methods
+using System.Collections.Generic;   // provides the generic list class
+
+
+namespace AnagramChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
