42e1432d7d8750c44dae3fbdbbee925c257f0d61
Diane Morrison <diane.e.morrison@gmail.com>
2022-11-04 22:11:26 +0000

    Basic iteration

--- Program.cs
+++ Program.cs
@@ -7,9 +7,32 @@ namespace AnagramChecker
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static void Main(string[] args)
+        {   
+     
+            //prompt user to create user name
+            Console.WriteLine("Welcome to the anagram checker!\n\nPlease enter a user name: ");
+            var userName = Console.ReadLine();
+
+            //prompt user for first text input and read from keyboard
+            Console.WriteLine($"Hello {userName}! Please enter your first word: ");
+            var firstWord = Console.ReadLine();
+            
+            //prompt user for second text input and read from keyboard
+            Console.WriteLine($"\n{userName}, your first word is {firstWord}. Please enter your second word: ");
+            var secondWord = Console.ReadLine();
+
+            
+
+
+
+
+        
+
+
+
+        
+
         }
     }
 }
